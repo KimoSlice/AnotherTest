d2bb41e343814bbafe0fef8c4b1b78610078c4dd
KimoSlice <karhegazy@gmail.com>
2020-05-21 12:57:46 +0200

    Add Project

---
+++ Assets/Scripts/PickUp.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp : MonoBehaviour
+{
+    public Transform holdPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 50.0f, Color.red);
+        CheckForPickUp();
+    }
+
+    private void CheckForPickUp()
+    {
+        RaycastHit hit;
+        if(Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, 20.0f))
+        {
+            if (Input.GetKey(KeyCode.Mouse0))
+            {
+                if (hit.transform.gameObject.GetComponent<Pickupable>())
+                {
+                    //pickup box
+                    hit.transform.gameObject.GetComponent<Pickupable>().SetPickedUpState(holdPosition);
+                }
+            }
+        }
+
+        if (Input.GetKey(KeyCode.Mouse1))
+        {
+            if (hit.transform.GetComponent<Pickupable>())
+            {
+                //Drop Box
+                hit.transform.GetComponent<Pickupable>().SetDroppedState();
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Plates/Pickupable.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickupable : MonoBehaviour
+{
+    Rigidbody rb;
+    public int boxId;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();   
+    }
+
+    public void SetPickedUpState(Transform holder)
+    {
+        rb.useGravity = false; //Stopps Box from fall
+        rb.isKinematic = true; //Stops Box interacting with other objects
+        transform.parent = holder;
+    }
+
+    public void SetDroppedState()
+    {
+        rb.useGravity = true;
+        rb.isKinematic = false;
+        transform.parent = null;
+    }
+
+    public int ReturnBoxId()
+    {
+        return boxId;
+    }
+}
