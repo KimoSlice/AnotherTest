951af9f817382dd5b834cd41c6ebb533bdc351e0
KimoSlice <karhegazy@gmail.com>
2020-06-14 22:15:17 +0200

    New Rooms

---
+++ Assets/Scripts/Plates/Hauptraum/PlateManager4.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class PlateManager4 : MonoBehaviour
+{
+    public static PlateManager4 instance;
+    [SerializeField]
+    int totalCorrectPlacementsNeed; //This is the total number of boxes that needs to be placed correctly before the door will open.
+    [SerializeField]
+    int currentCorrectPlacements; //Current number of boxes that are placed on the correct pad
+    [SerializeField]
+    int placements = 0; //This is overall attempted placements. This increments everytime a box is placed on a pad
+
+    public List<GameObject> pads;
+    //public List<GameObject> boxes;
+    //public List<GameObject> differentType;
+
+    public Text canvasText;
+
+    public UnityEvent completeEvent; //The event you want to call when all boxes are placed. This can be anything. 
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        totalCorrectPlacementsNeed = pads.Count; //Set the total number of correct placements needed to be the number of pads in the pads list. 
+        currentCorrectPlacements = 0; //Start off with 0 correct placements
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    /// <summary>
+    /// Increase the number of attempted placements
+    /// </summary>
+    public void IncreasePlacement()
+    {
+        placements++;
+
+        if (placements == totalCorrectPlacementsNeed) //Update the UI board
+        {
+            currentCorrectPlacements.ToString();
+        }
+    }
+
+    /// <summary>
+    /// Decrease the number of attempted placements
+    /// </summary>
+    public void DecreasePlacement()
+    {
+        placements--;
+    }
+
+    /// <summary>
+    /// Increase the number of CORRECT placements
+    /// </summary>
+    public void IncreaseCorrectPlacements()
+    {
+        currentCorrectPlacements++;
+
+        if (currentCorrectPlacements == totalCorrectPlacementsNeed)
+        {
+            Debug.Log("ALL BOXES PLACED CORRECTLY");
+            completeEvent.Invoke();
+        }
+    }
+
+    /// <summary>
+    /// Decrease the number of incorrect placements
+    /// </summary>
+    public void DecreaseCorrectPlacements()
+    {
+        currentCorrectPlacements--;
+    }
+}
---
+++ Assets/Scripts/Plates/Hauptraum/pad4.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pad4 : MonoBehaviour
+{
+    public int padId;
+    int objectsOnPad = 0;
+
+    /// <summary>
+    /// Called whenever a collision occurs with this pad
+    /// </summary>
+    /// <param name="other"></param>
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.GetComponent<Pickupable>() != null) //If a box is placed...
+        {
+            objectsOnPad++;
+
+
+            if (other.gameObject.GetComponent<Pickupable>().ReturnBoxId() == padId) //If the box has the same number as the pad, then its the correct box...
+            {
+
+                PlateManager4.instance.IncreaseCorrectPlacements(); //so increment the correct placements
+
+            }
+
+            PlateManager4.instance.IncreasePlacement(); //Increase the number of placement attempts (does not have to be a correct placement)
+        }
+    }
+
+    /// <summary>
+    /// Called when a colliding objects stops colliding with the pad
+    /// </summary>
+    /// <param name="other"></param>
+    private void OnTriggerExit(Collider other)
+    {
+
+        if (other.gameObject.GetComponent<Pickupable>() != null) //If it is a box that has stopped colliding...
+        {
+            objectsOnPad--; //...then reduce the number of colliding objects...
+            PlateManager4.instance.DecreasePlacement();
+
+
+            if (other.gameObject.GetComponent<Pickupable>().ReturnBoxId() == padId) //...If the box that has stopped colliding is the correct box for this pad...
+            {
+
+                PlateManager4.instance.DecreaseCorrectPlacements(); //...then decrease the number of corectly placed pads 
+            }
+        }
+
+    }
+}
